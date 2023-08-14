a5802f7a820cc17266a015e0a5f1f291c31924e1
elkarouani <elkarouani@gmail.com>
2023-08-14 12:50:38 -0700

    Task: 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for cell in row:
+            print("{:d}".format(cell), end = ' ' if cell != row[-1] else '')
+        print()
