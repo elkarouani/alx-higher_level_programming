f7ebb6042b556b5e4f58c5fdb1478a82647334aa
elkarouani <elkarouani@gmail.com>
2023-08-14 13:16:14 -0700

    Task: 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,6 +6,7 @@ def max_integer(my_list=[]):
 
     max_int = my_list[0]
     for n in my_list:
-        if n > max_int: max_int = n
+        if n > max_int:
+            max_int = n
 
     return max_int
