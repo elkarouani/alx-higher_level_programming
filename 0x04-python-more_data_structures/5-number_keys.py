7f4d08a0caa16e6d3d402b835f70e817b521b713
elkarouani <elkarouani@gmail.com>
2023-08-18 12:21:59 -0700

    Task: 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictionary):
-    count = 0
-    for i in list(a_dictionary.keys()):
-        count += i
-    return count
+    return len(a_dictionary.keys())
