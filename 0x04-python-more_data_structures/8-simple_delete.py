3cd9baf5dbaf95f018724a750e0fd290100cfa29
elkarouani <elkarouani@gmail.com>
2023-08-18 12:34:50 -0700

    Task: 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if dictionary.get(key) is not None:
+    if a_dictionary.get(key) is not None:
         del a_dictionary[key]
     return a_dictionary
