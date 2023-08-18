2ae2f984b6ee76a0432d7d1c6522e5f3eed168a7
elkarouani <elkarouani@gmail.com>
2023-08-18 13:49:51 -0700

    Task: 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not roman_string and not isinstance(roman_string, str):
+    if not roman_string or not isinstance(roman_string, str):
         return 0
 
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
