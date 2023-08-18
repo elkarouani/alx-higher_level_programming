d6b784eb43a836f2b7645f5f63183a1a4f0769ca
elkarouani <elkarouani@gmail.com>
2023-08-18 14:00:15 -0700

    Task: 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,10 @@ def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+    }
     result = 0
     pass_current = False
 
@@ -14,7 +17,10 @@ def roman_to_int(roman_string):
             continue
 
         current_value = roman_dict[roman_string[i]]
-        next_value = 0 if i+1 == len(roman_string) else roman_dict[roman_string[i+1]]
+        next_value = 0
+
+        if i + 1 < len(roman_string):
+            next_value = roman_dict[roman_string[i+1]]
 
         if current_value < next_value:
             result += next_value - current_value
